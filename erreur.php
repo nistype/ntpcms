5e1ed63a9a90b889846b52277a48a27ccbb21c90
nistype <52999108+nistype@users.noreply.github.com>
2020-04-30 22:03:39 +0200

    Add files via upload

---
+++ erreur.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head> 
+  <meta charset="utf-8" /> 
+  <title>Habbaw - Erreur</title> 
+  <meta name="viewport" content="width=device-width, initial-scale=1" /> 
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script> 
+  <link href="http://88.99.153.133/erreur/css/main.css" rel="stylesheet" /> 
+  <link href="http://88.99.153.133/erreur/css/style.css" type="text/css" rel="stylesheet" media="all" /> 
+ </head> 
+ <body> 
+  <center><div class="container bootstrap snippet"> 
+   <div class="row"> 
+    <div class="col-md-12"> 
+      <div class="col-md-10 col-md-offset-1"> 
+       <img class="img-error" src="http://88.99.153.133/erreur/images/error.gif" /> 
+       <h2>404 Not Found</h2> 
+          <p><b>Désolé, il semble avoir une erreur!</b></p> 
+       <div class="error-actions"> 
+        <a href="http://88.99.153.133/me" class="btn btn-primary btn-lg"><img src="http://habboemotion.com/resources/images/icons/v20_8.gif"/></span> Retour CMS </a> </center>
+       </div> 
+      </div> 
+     </div> 
+    </div> 
+   </div> 
+  </div> 
+  <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+ </body>
+</html>
---
+++ maintenance.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head> 
+  <meta charset="utf-8" /> 
+  <title>Habbaw - Maintenance</title> 
+  <meta name="viewport" content="width=device-width, initial-scale=1" /> 
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script> 
+  <link href="http://88.99.153.133/erreur/css/main.css" rel="stylesheet" /> 
+  <link href="http://88.99.153.133/erreur/css/style.css" type="text/css" rel="stylesheet" media="all" /> 
+ </head> 
+ <body> 
+  <center><div class="container bootstrap snippet"> 
+   <div class="row"> 
+    <div class="col-md-12"> 
+      <div class="col-md-10 col-md-offset-1"> 
+       <img class="img-error" src="http://88.99.153.133/erreur/images/maintenance.gif" /> 
+          <h2><b>Maintenance</b></h2> 
+          <p><b>Repasse plus tard pour l'ouverture d'Habbaw</b></p> 
+          <br>
+       <div class="error-actions"> 
+        <a href="https://discord.gg/dVadPJ3" class="btn btn-primary btn-lg"><img src="http://habboemotion.com/resources/images/icons/v20_8.gif"/></span> Rejoindre le discord </a> </center>
+       </div> 
+      </div> 
+     </div> 
+    </div> 
+   </div> 
+  </div> 
+  <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+ </body>
+</html>
