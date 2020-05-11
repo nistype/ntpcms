7468a2960807a0879011b76c82053b5db344e341
nistype <52999108+nistype@users.noreply.github.com>
2020-05-11 16:45:26 +0200

    Update core.php

--- config/core.php
+++ config/core.php
@@ -31,22 +31,7 @@
         'vip_points' => '4',
         'motto' => '',
         'look' => 'hd-180-2.hr-891-45.ha-1004-110.he-3376-92.fa-3296-62.ch-255-71.lg-270-110.sh-290-71.wa-3074-92'
-    );
-
-$zan_settings = [
-	"name" => "",
-	"twitter" => "",
-	"facebook" => "",
-	"weblink" => "",
-	"logo" => "",
-	"slogan" => "Love",
-	"motto" => "",
-	"credits" => 50000,
-	"diamonds" => 0,
-	"duckets" => 50000,
-	"chat_id" => "156095602", // ID de xat.com
-	"max_rank" => 8
-];
+    ]);
 
 
     if(isset($_SESSION['id'])){
@@ -59,7 +44,7 @@ $zan_settings = [
     {
         $ban = mysqli_fetch_assoc($l_ban);
         echo '<div id="error" style="background:#A40101; color:#FFFFFF; width:750px;margin-left:auto;margin-right:auto; height:23px; line-height:23px; font-size:17px; text-align:center; margin-top:-4px;">
-        Error: Tu es exclu pour la raison: '.$ban['reason'].' !
+        Erreur: Tu es exclu pour la raison: '.$ban['reason'].' !
         </div>';
         $loginerror = true;
     }
